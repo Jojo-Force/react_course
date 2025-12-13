27d31b905c02e5ca279292b63f19d99692b3c5a4
Jojo <jojo_zeng@126.com>
2025-12-13 09:21:42 +0800

    update

--- 02-JS-review/script.js
+++ 02-JS-review/script.js
@@ -288,7 +288,18 @@ const booksAfterUpdate = booksAfterDelete.map((book) =>
 booksAfterUpdate;
 */
 
-fetch("https://jsonplaceholder.typicode.com/todos")
-  .then((res) => res.json())
-  .then((data) => console.log(data));
+// fetch("https://jsonplaceholder.typicode.com/todos")
+//   .then((res) => res.json())
+//   .then((data) => console.log(data));
+// console.log("jojo");
+
+async function getTools() {
+  const res = await fetch("https://jsonplaceholder.typicode.com/todos");
+  const data = await res.json();
+  console.log(data);
+  return data;
+}
+
+const todos = await getTools();
+console.log(todos);
 console.log("jojo");
