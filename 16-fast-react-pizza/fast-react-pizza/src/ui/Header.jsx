1c3fb7f267fd7b802f510761d34134431508d9ff
jojo <jojo_zeng@126.com>
2026-01-07 20:01:16 +0800

    update

--- 16-fast-react-pizza/fast-react-pizza/src/ui/Header.jsx
+++ 16-fast-react-pizza/fast-react-pizza/src/ui/Header.jsx
@@ -1,11 +1,15 @@
 import { Link } from 'react-router-dom';
 import SearchOrder from '../features/order/SearchOrder';
+import Username from '../features/user/Username';
 
 function Header() {
   return (
-    <header className="bg-yellow-500">
-      <Link to="/">Fast React Pizza Co.</Link>
+    <header className="border-b-2 border-stone-200 bg-yellow-500 px-4 py-3 uppercase">
+      <Link to="/" className="tracking-widest">
+        Fast React Pizza Co.
+      </Link>
       <SearchOrder />
+      <Username />
     </header>
   );
 }
