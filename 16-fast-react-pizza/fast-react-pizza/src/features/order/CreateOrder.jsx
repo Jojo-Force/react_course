097ad20f35a7f87a5f9c8fa6f339b858ea432078
jojo <jojo_zeng@126.com>
2026-01-08 17:29:31 +0800

    update

--- 16-fast-react-pizza/fast-react-pizza/src/features/order/CreateOrder.jsx
+++ 16-fast-react-pizza/fast-react-pizza/src/features/order/CreateOrder.jsx
@@ -3,6 +3,8 @@ import { Form, redirect, useActionData } from 'react-router-dom';
 import { createOrder } from '../../services/apiRestaurant';
 import { useNavigation } from 'react-router-dom';
 import Button from '../../ui/Button';
+import { useSelector } from 'react-redux';
+
 // https://uibakery.io/regex-library/phone-number
 const isValidPhone = (str) =>
   /^\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}$/.test(
@@ -34,6 +36,8 @@ const fakeCart = [
 ];
 
 function CreateOrder() {
+  const username = useSelector((state) => state.user.username);
+
   const navigation = useNavigation();
   const isSubmitting = navigation.state === 'submitting';
   // const [withPriority, setWithPriority] = useState(false);
@@ -50,7 +54,13 @@ function CreateOrder() {
       <Form method="POST">
         <div className="mb-5 flex flex-col gap-2 sm:flex-row sm:items-center">
           <label className="sm:basis-40">First Name</label>
-          <input className="input grow" type="text" name="customer" required />
+          <input
+            className="input grow"
+            type="text"
+            name="customer"
+            defaultValue={username}
+            required
+          />
         </div>
 
         <div className="mb-5 flex flex-col gap-2 sm:flex-row sm:items-center">
