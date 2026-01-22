f8f3bc9d7f397cff407bfc9e4e42a013ef88b40f
jojo <jojo_zeng@126.com>
2026-01-22 19:05:17 +0800

    update

--- 17-the-wild-oasis/src/features/cabins/CabinRow.jsx
+++ 17-the-wild-oasis/src/features/cabins/CabinRow.jsx
@@ -1,4 +1,7 @@
 import styled from "styled-components";
+import { formatCurrency } from "../../utils/helpers";
+import { useMutation } from "@tanstack/react-query";
+import { deleteCabin } from "../../services/apiCabins";
 
 const TableRow = styled.div`
   display: grid;
@@ -40,9 +43,31 @@ const Discount = styled.div`
 `;
 
 function CabinRow({ cabin }) {
-  const { name, maxCapacity, regularPrice, discount, image } = cabin;
+  const {
+    id: cabinId,
+    name,
+    maxCapacity,
+    regularPrice,
+    discount,
+    image,
+  } = cabin;
 
-  return <TableRow role="row">{}</TableRow>;
+  const { isLoading: isDeleting, mutate } = useMutation({
+    mutationFn: deleteCabin,
+  });
+
+  return (
+    <TableRow role="row">
+      <Img src={image} />
+      <Cabin>{name}</Cabin>
+      <div>Fits up tp {maxCapacity}</div>
+      <Price>{formatCurrency(regularPrice)}</Price>
+      <Discount>{formatCurrency(discount)}</Discount>
+      <button onClick={() => mutate(cabinId)} disabled={isDeleting}>
+        Delete
+      </button>
+    </TableRow>
+  );
 }
 
 export default CabinRow;
